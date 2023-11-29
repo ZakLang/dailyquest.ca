5a45f86634257fb1b18482dbb16098a37e4a2a7a
Zak Lang <zachary.w.lang@gmail.com>
2023-11-28 17:01:46 -0800

    Fix Bug

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -1,5 +1,5 @@
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/NavBar';
+import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Logo from './Logo';
 
